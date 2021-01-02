3283f4b52e4fec6d334e52ee46d8a6a28822b8ba
Daymen <daymen88@gmail.com>
2021-01-02 14:43:21 -0800

    DisplayingValues

---
+++ src/components/people-search/DisplaySearch.js
@@ -0,0 +1,6 @@
+function DisplaySearch(people) {
+  console.log(people);
+  return <div>DisplaySearch Component</div>;
+}
+
+export default DisplaySearch;
--- src/components/people-search/PeopleSearch.js
+++ src/components/people-search/PeopleSearch.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
-import { ButtonGroup } from "react-bootstrap";
+import DisplaySearch from "./DisplaySearch";
+import Card from "react-bootstrap/Card";
 
 function PeopleSearch() {
   const [people, setPeople] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState("Luke");
   const [searchValue, setSearchValue] = useState("");
 
@@ -15,22 +16,33 @@ function PeopleSearch() {
       .get("https://swapi.dev/api/people/", { params: { search: query } })
       .then((response) => {
         setPeople(response.data);
-        console.log(response.data);
         setIsLoading(false);
       })
       .catch((err) => {
-        console.log(err);
         setIsLoading(false);
       });
   }, [query]);
 
-
   return (
     <div>
-      <input placeholder="Enter character name" onChange={(e) =>{
-          setSearchValue(e.target.value)
-      }}></input>
-      <Button onClick={() =>{setQuery(searchValue)}}>Click me</Button>
+      <input
+        placeholder="Enter character name"
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      ></input>
+      <Button
+        onClick={() => {
+          setQuery(searchValue);
+        }}
+      >
+        Click me
+      </Button>
+      <div>
+      {isLoading ? "Loading ..." : <Card>{people.results.map((peeps) =>{
+        return <Card.Body key={peeps.name}>{peeps.name}</Card.Body>
+      })}</Card>}
+      </div>
     </div>
   );
 }
