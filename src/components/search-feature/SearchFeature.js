d2046d60cfdeca6ac6e6802b560f4b9810536163
Daymen Deol <daymen88@gmail.com>
2021-01-03 20:51:07 -0800

    Added basic pagination funcationality

--- src/components/search-feature/SearchFeature.js
+++ src/components/search-feature/SearchFeature.js
@@ -4,20 +4,42 @@ import Button from "react-bootstrap/Button";
 import SearchResults from "./SearchResults";
 
 function SearchFeature() {
+  // People api
   const [people, setPeople] = useState([]);
   const [query, setQuery] = useState("Luke");
   const [searchValue, setSearchValue] = useState("");
+  const [nextPage, setNextPage] = useState("");
+  const [previousPage, setPreviousPage] = useState("");
+  const [url, setUrl] = useState("https://swapi.dev/api/people/");
+
+  // Planet api
+  const [planets, setPlanets] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get("https://swapi.dev/api/people/", {
+      const response = await axios.get(url, {
         params: { search: query },
       });
       console.log(response.data);
       setPeople(response.data.results);
+      setNextPage(response.data.next);
+      setPreviousPage(response.data.previous);
     };
     fetchData();
-  }, [query]);
+  }, [query, url]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://swapi.dev/api/planets");
+      console.log(response.data);
+      setPlanets(
+        response.data.results.map((planet) => {
+          return { name: planet.name, url: planet.url };
+        })
+      );
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -34,7 +56,25 @@ function SearchFeature() {
       >
         Search
       </Button>
-      <SearchResults people={people} />
+      <Button
+        onClick={() => {
+          if (nextPage) {
+            setUrl(nextPage);
+          }
+        }}
+      >
+        Next Page
+      </Button>
+      <Button
+        onClick={() => {
+          if (previousPage) {
+            setUrl(previousPage);
+          }
+        }}
+      >
+        Previous Page
+      </Button>
+      <SearchResults people={people} planets={planets} />
     </div>
   );
 }
--- src/components/search-feature/SearchResults.js
+++ src/components/search-feature/SearchResults.js
@@ -1,8 +1,11 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./SearchFeature.css";
 
 function SearchResults(props) {
-  const people = props.people;
+  const { people, planets } = props;
+
   return (
     <div>
       <Card.Title>Testing</Card.Title>
@@ -12,7 +15,16 @@ function SearchResults(props) {
             <Card.Header>{peeps.name}</Card.Header>
             <ListGroup>
               <ListGroup.Item>Birth Year: {peeps.birth_year}</ListGroup.Item>
-              <ListGroup.Item>Home world: {peeps.homeworld}</ListGroup.Item>
+              <ListGroup.Item>
+                Home world:
+                {` ${planets
+                  .filter((planet) => {
+                    return planet.url === peeps.homeworld;
+                  })
+                  .map((something) => {
+                    return something.name;
+                  })}`}
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         );
