cbbe6f65d368004a42523592ec44e65065cdccfb
Daymen Deol <daymen88@gmail.com>
2021-01-05 18:27:57 -0800

    Disabled search once api has been called

--- src/components/search-feature/SearchFeature.js
+++ src/components/search-feature/SearchFeature.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import SearchResults from "./SearchResults";
 
@@ -7,7 +7,6 @@ function SearchFeature() {
   // People api
   const [people, setPeople] = useState([]);
   const [query, setQuery] = useState("Luke");
-  const [searchValue, setSearchValue] = useState("");
   const [nextPage, setNextPage] = useState("");
   const [previousPage, setPreviousPage] = useState("");
   const [url, setUrl] = useState("https://swapi.dev/api/people/");
@@ -15,12 +14,16 @@ function SearchFeature() {
   // Planet api
   const [planets, setPlanets] = useState([]);
 
+  // Disabling Search button to avoid repeated api calls.
+  const [fetchPlanets, setFetchPlanets] = useState(false);
+  const [searchButtonDisabled, setSearchButtonDisabled] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(url, {
         params: { search: query },
       });
-      console.log(response.data);
+      //console.log(response.data);
       setPeople(response.data.results);
       setNextPage(response.data.next);
       setPreviousPage(response.data.previous);
@@ -29,30 +32,36 @@ function SearchFeature() {
   }, [query, url]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("http://swapi.dev/api/planets");
-      console.log(response.data);
-      setPlanets(
-        response.data.results.map((planet) => {
-          return { name: planet.name, url: planet.url };
-        })
-      );
-    };
-    fetchData();
-  }, []);
+    if (fetchPlanets) {
+      const fetchData = async () => {
+        const response = await axios.get("http://swapi.dev/api/planets");
+        //console.log(response.data);
+        setPlanets(
+          response.data.results.map((planet) => {
+            return { name: planet.name, url: planet.url };
+          })
+        );
+        setFetchPlanets(false);
+        setSearchButtonDisabled(false);
+      };
+      fetchData();
+    }
+  }, [fetchPlanets]);
+
+  const characterInputRef = useRef();
 
   return (
     <div>
-      <input
-        onChange={(e) => {
-          setSearchValue(e.target.value);
-        }}
-        placeholder="Enter character name"
-      ></input>
+      <input ref={characterInputRef} placeholder="Enter character name"></input>
       <Button
         onClick={() => {
-          setQuery(searchValue);
+          if (!fetchPlanets) {
+            setSearchButtonDisabled(true);
+            setQuery(characterInputRef.current.value);
+            setFetchPlanets(true);
+          }
         }}
+        disabled={searchButtonDisabled}
       >
         Search
       </Button>
